3ff835a3b6e3b79ecf7694f1cf95625afeed2d92
Laura Kressin <laurakressin@gmail.com>
2016-07-19 21:06:16 -0500

    initial xml importer

---
+++ DataWrangling/ch3/import_xml.py
@@ -0,0 +1,27 @@
+from xml.etree import ElementTree as ET
+
+tree - ET.parse(../data-wrangling-master/data/chp3/data-text.xml')
+root = tree.getroot()
+
+data = root.find('Data')
+
+all_data = []
+
+for observation in data:
+    record = {}
+    for item in observation:
+
+        lookup_key = item.attrib.keys()[0]
+
+        if lookup_key == 'Numeric':
+            rec_key = 'Numeric'
+            rec_value = item.attib['Numeric']
+        else:
+            rec_key = item.attrib[lookup_key]
+            rec_key = item.attrib['Code']
+
+        record[rec_key] = rec_value
+
+    all_data.append(record)
+
+print all_data

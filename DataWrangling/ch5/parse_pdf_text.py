2774594d5f196cd203905c54ec03e4a62b530986
Laura Kressin <laurakressin@gmail.com>
2016-07-31 21:32:02 -0500

    adding total_line

--- DataWrangling/ch5/parse_pdf_text.py
+++ DataWrangling/ch5/parse_pdf_text.py
@@ -1,9 +1,9 @@
 pdf_txt = 'en-final-table9.txt'
 openfile = open(pdf_txt, 'r')
 
-country_line = False
+country_line = total_line = False
 for line in openfile:
-    if country_line:
+    if country_line or total_line:
         print line
 
     if line.startswith('and areas'):
@@ -11,3 +11,9 @@ for line in openfile:
     elif country_line:
         if line == '/n':
             country_line = False
+
+    if line.startswith('total'):
+        total_line = True
+    elif total_line:
+        if line == '\n':
+            total_line = False

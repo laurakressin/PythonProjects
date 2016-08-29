1dc19febc23d311b8306c30f7ff640db95fa0e84
Laura Kressin <laurakressin@gmail.com>
2016-08-29 13:28:43 -0500

    adding comments

--- TheHardWay/ex19.py
+++ TheHardWay/ex19.py
@@ -8,14 +8,17 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
 print "We can just give the function numbers directly:"
 cheese_and_crackers(20, 30)
 
+# assigning numbers to arguments
 print "OR, we can use variables from our script:"
 amount_of_cheese = 10
 amount_of_crackers = 50
 
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
+# assigning addition problems to arguments
 print "We can even do math inside too:"
 cheese_and_crackers(10 + 20, 5 + 6)
 
+# adding number to already assigned variables
 print "And we can combine the two, variables and math:"
 cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)

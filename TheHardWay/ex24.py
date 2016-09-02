5a7d41b29137b2bc2f17e107cdc906b2f6070adb
Laura Kressin <laurakressin@gmail.com>
2016-09-02 09:36:59 -0500

    ex 24 poem

---
+++ TheHardWay/ex24.py
@@ -0,0 +1,11 @@
+print "Let's practive everything."
+print 'You\'d need to know \'bout excapes with \\ that do \n newlines and \t tabs.'
+
+poem = """
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+"""
